ee534114ec4750dfec369d9b956e2faf062df571
chris-yi <kri5to.yi@gmail.com>
2018-05-29 20:42:08 -0600

    couple other implementaions of bubble sort

--- Bubble Sort/bubbleSort.js
+++ Bubble Sort/bubbleSort.js
@@ -58,3 +58,52 @@ const bubbleSort = (nums) => {
 /*
 1.  BIG O is n^2 for bubble sort
 */
+
+
+///////////////////////////////////////////
+// const bubbleSort = (arr) => {
+//     do {
+//         var swapped = false;
+//         arr.forEach((num, i) => {
+//             if(num > arr[i + 1]) {
+//                 let temp = num;
+//                 arr[i] = arr[i + 1];
+//                 arr[i + 1] = temp;
+//                 swapped = true;
+//             }
+//         })
+//     }while(swapped === true)
+//     return arr;
+// }
+///////////////////////////////////////////
+
+
+function bubbleSort(array) {
+
+    // Use an isSorted flag to determine whether or not there
+    // is more sorting to do when we are in the while loop
+    let isSorted = false;
+
+    while(!isSorted) {
+
+        // Mark is sorted true, then re-mark it false in the loop.
+        // If the loop is finished, then isSorted will stay true
+        // and we will thus exit the while loop and return
+        isSorted = true;
+
+        for(let i = 0; i < array.length - 1; i++) {
+            if(array[i] > array[i+1]) {
+                // Swap by holding the first element in a temp variable,
+                // then reassigning indexes i and i+1 to each other
+                let temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+
+                // Mark the isSorted flag false because there is still sorting to do if we are in the loop
+                isSorted = false;
+            }
+        }
+    }
+    // Return the sorted array!
+    return array;
+}
