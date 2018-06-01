1b99060f54559d1ff2686b3bbb40e0b5469a2bb3
chris-yi <kri5to.yi@gmail.com>
2018-05-31 18:35:32 -0600

    Merge sort

---
+++ Merge Sort/mergeSort.js
@@ -0,0 +1,33 @@
+// ****** Very basic example of a Merge Sort: ****** //
+
+
+
+
+// ****** END OF basic example of Merge Sort ****** //
+
+
+
+
+
+
+// ****** EXERCISE 1 ****** //
+
+
+
+
+// ****** END OF Exercise 1 ****** //
+
+
+
+
+// ****** Merge Sort NOTES ****** //
+/*
+
+1.  Merge sort is the one most often used.
+
+2. When you use somthing like array.sort(), 90% of the time, under the hood, merge sort is being utilized.
+
+
+*/
+
+// ****** END OF Merge Sort NOTES ****** //
