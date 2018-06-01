0c2d6b5b75877e835a68cc0eb7158eab1b34e98c
chris-yi <kri5to.yi@gmail.com>
2018-05-31 18:29:01 -0600

    solution for insertion sort

--- Sorting Solutions/sortingSolutions.js
+++ Sorting Solutions/sortingSolutions.js
@@ -17,3 +17,21 @@ const bubbleSort = (arr) => {
 }
 
 // ************ END OF Bubble Sort ************ //
+
+
+// ************ Insertion Sort ************ //
+
+const insertionSort = (arr) => {
+    for(let i = 1; i < arr.length; i++) {
+        for(let j = 0; j < i; j++) {
+            console.log(`[UNSORTED]`, arr)
+            if(arr[i] < arr[j]) {
+                const spliced = arr.splice(i, 1);
+                arr.splice(j, 0, spliced[0]);
+            }
+        }
+    }
+    return arr;
+}
+
+// ************ END OF Insertion Sort ************ //
