4b8f9c65cd904243a0fc81ff02589ba6bace1699
chris-yi <kri5to.yi@gmail.com>
2018-06-03 17:16:04 -0600

    merge sort

--- Merge Sort/mergeSort.js
+++ Merge Sort/mergeSort.js
@@ -1,45 +1,78 @@
 // ****** EXERCISE 1 ****** //
 
-const mergeSort = (nums) => {
-    if(nums.length < 2) {
-        return nums;
-    }
+const mergeSort = nums => {
+  if (nums.length < 2) {
+    return nums;
+  }
 
-    const length = nums.length;
-    const middle = Math.floor(length / 2);
-    const left = nums.slice(0, middle);
-    const right = nums.slice(middle, length);
+  const length = nums.length;
+  const middle = Math.floor(length / 2);
+  const left = nums.slice(0, middle);
+  const right = nums.slice(middle, length);
 
-    const sortedLeft = mergeSort(left);
-    const sortedRight = mergeSort(right);
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
 
-    return stitch(sortedLeft, sortedRight);
+  return stitch(sortedLeft, sortedRight);
 };
 
 const stitch = (left, right) => {
-    const results = [];
+  const results = [];
 
-    while(left.length && right.length) {
-        if(left[0] <= right[0]) {
-            results.push(left.shift());
-        } else {
-            results.push(right.shift());
-        }
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
     }
-    while(left.length) {
-        results.push(left.shift());
-    }
-    while(right.length) {
-        results.push(right.shift());
-    }
+  }
+  while (left.length) {
+    results.push(left.shift());
+  }
+  while (right.length) {
+    results.push(right.shift());
+  }
 
-    return results;
-}
+  return results;
+};
 
 // ****** END OF Exercise 1 ****** //
 
+const mergeSort = arr => {
+  if (arr.length < 2) {
+    return arr;
+  }
 
+  const length = arr.length;
+  const mid = Math.floor(length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid, length);
 
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return stitch(sortedLeft, sortedRight);
+};
+
+const stitch = (left, right) => {
+  const result = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  while (left.length) {
+    result.push(left.shift());
+  }
+  while (right.length) {
+    result.push(right.shift());
+  }
+  return result;
+};
 
 // ****** Merge Sort NOTES ****** //
 /*
--- Sorting Solutions/sortingSolutions.js
+++ Sorting Solutions/sortingSolutions.js
@@ -1,37 +1,76 @@
 // ************ Bubble Sort ************ //
 
-const bubbleSort = (arr) => {
-    do{
-        var sorted = false;
-        arr.forEach((num, i) => {
-            console.log(`[UNSORTED]`, arr)
-            if(num > arr[i + 1]){
-                let temp = num;
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                sorted = true;
-            }
-        })
-    }while(sorted)
-    return arr;
-}
+const bubbleSort = arr => {
+  do {
+    var sorted = false;
+    arr.forEach((num, i) => {
+      console.log(`[UNSORTED]`, arr);
+      if (num > arr[i + 1]) {
+        let temp = num;
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        sorted = true;
+      }
+    });
+  } while (sorted);
+  return arr;
+};
 
 // ************ END OF Bubble Sort ************ //
 
-
 // ************ Insertion Sort ************ //
 
-const insertionSort = (arr) => {
-    for(let i = 1; i < arr.length; i++) {
-        for(let j = 0; j < i; j++) {
-            console.log(`[UNSORTED]`, arr)
-            if(arr[i] < arr[j]) {
-                const spliced = arr.splice(i, 1);
-                arr.splice(j, 0, spliced[0]);
-            }
-        }
+const insertionSort = arr => {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      console.log(`[UNSORTED]`, arr);
+      if (arr[i] < arr[j]) {
+        const spliced = arr.splice(i, 1);
+        arr.splice(j, 0, spliced[0]);
+      }
     }
-    return arr;
-}
+  }
+  return arr;
+};
 
 // ************ END OF Insertion Sort ************ //
+
+// ************ Merge Sort ************ //
+
+const mergeSort = arr => {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  const length = arr.length;
+  const mid = Math.floor(length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid, length);
+
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return stitch(sortedLeft, sortedRight);
+};
+
+const stitch = (left, right) => {
+  const result = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  while (left.length) {
+    result.push(left.shift());
+  }
+  while (right.length) {
+    result.push(right.shift());
+  }
+  return result;
+};
+
+// ************ END OF Merge Sort ************ //
