76aeb1b15198555eeb1c720b4e6dcb59d15467f6
chris-yi <kri5to.yi@gmail.com>
2018-06-09 20:30:38 -0600

    notes for maps

--- Data Structure/sets.js
+++ Data Structure/sets.js
@@ -1,13 +1,17 @@
 // ****** Set Data NOTES ****** //
 /*
 
-1.  Merge sort is the one most often used.
+1.  Set and data are completely different.  A set is more like an object.
 
-2. When you use somthing like array.sort(), 90% of the time, under the hood, merge sort is being utilized.
+2. You can think of it as a cloud, you run set.add(5), it'll just add a 5 into the pool.
 
-3.  Reason why merge sort is commonly used is because its consistent.
+3.  A set cannot have any duplicates, so if you add 5 again, it will do nothing.
 
-4.  Merge sort uses Recursion.
+4.  Very useful when you have a large data set that you want to take our all the duplicates.
+
+5.  Case: Username, cannot have duplicates of usernames.
+
+6.  A set allows at least four things, add, remove, contains and toList.
 
 */
 
