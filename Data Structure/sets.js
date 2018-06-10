0b52d5cdd5995e7974c3fe1e9d15c2a813d9d3af
chris-yi <kri5to.yi@gmail.com>
2018-06-09 20:26:36 -0600

    added sets notes

---
+++ Data Structure/sets.js
@@ -0,0 +1,14 @@
+// ****** Set Data NOTES ****** //
+/*
+
+1.  Merge sort is the one most often used.
+
+2. When you use somthing like array.sort(), 90% of the time, under the hood, merge sort is being utilized.
+
+3.  Reason why merge sort is commonly used is because its consistent.
+
+4.  Merge sort uses Recursion.
+
+*/
+
+// ****** END OF Set Data NOTES ****** //
