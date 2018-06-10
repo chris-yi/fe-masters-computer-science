6b571eb8a553802991ced9d067040a43c10d8a1c
chris-yi <kri5to.yi@gmail.com>
2018-06-09 20:39:50 -0600

    stack notes

---
+++ Data Structure/map.js
@@ -0,0 +1,18 @@
+// ****** Map Data NOTES ****** //
+/*
+
+1.  Maps are also known as Dictionaries
+
+2. Has a set of keys are their corresponding values
+
+3. Similar to objects in JS, but maps dont have prototypes, inheritance, methods or anything of that sort.
+
+4. Just key, values
+
+5. Maps dont have orders
+
+6. The keys are sets
+
+*/
+
+// ****** END OF Map Data NOTES ****** //
---
+++ Data Structure/stack.js
@@ -0,0 +1,14 @@
+// ****** Stack Data NOTES ****** //
+/*
+
+1.  Stack is an interface that adheres to "Last-in First-out"
+
+2.  Push and Pop, which is stack terminology
+
+3.  They'll often have a method called peek which just looks at the top value of the stack without modifying the stack
+
+4.  We use stack all the time, because Javascript is run on stacks
+
+*/
+
+// ****** END OF Stack Data NOTES ****** //
