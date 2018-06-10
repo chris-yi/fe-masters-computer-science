c37a48240d02d41201141126246c14d67b0826df
chris-yi <kri5to.yi@gmail.com>
2018-06-09 20:44:51 -0600

    Queue notes

---
+++ Data Structure Interfaces/queue.js
@@ -0,0 +1,12 @@
+// ****** Queue Data NOTES ****** //
+/*
+
+1.  Queue is "First-in First-Out"
+
+2.  Its like a line, person that was standing in line the longest first goes out
+
+3.  All stacks need to have the methods enqueue(add/push) and dequeue(remove/pop)
+
+*/
+
+// ****** END OF Queue Data NOTES ****** //
